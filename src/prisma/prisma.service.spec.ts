f4a43050fd7752a16660e76bfdd6e7975a3ce511
Hussain Almeshaima <hussainbh00@gmail.com>
2024-07-13 18:42:03 +0300

    fix: add prisma service test

---
+++ src/prisma/prisma.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('PrismaService', () => {
+  let service: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        {
+          provide: PrismaService,
+          useValue: {
+            search: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
