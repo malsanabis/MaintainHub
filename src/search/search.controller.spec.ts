de502c63f529c54e26c8a5614b61aa3ad83048f4
Hussain Almeshaima <hussainbh00@gmail.com>
2024-07-13 18:40:58 +0300

    fix: cover search controller

--- src/search/search.controller.spec.ts
+++ src/search/search.controller.spec.ts
@@ -7,16 +7,36 @@ describe('SearchController', () => {
   let controller: SearchController;
   let prismaService: PrismaService;
 
+  const mockProducts = {
+    data: [
+      {
+        name: 'Product 1',
+        code: 'P001',
+        uuid: 'uuid1',
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Product 2',
+        code: 'P002',
+        uuid: 'uuid2',
+        updatedAt: new Date(),
+      },
+    ],
+  };
+
   beforeEach(async () => {
+    const mockPrismaService = {
+      product: {
+        findMany: jest.fn().mockResolvedValue(mockProducts),
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SearchController],
       providers: [
         SearchService,
         {
           provide: PrismaService,
-          useValue: {
-            search: jest.fn(),
-          },
+          useValue: mockPrismaService,
         },
       ],
     }).compile();
@@ -27,5 +47,33 @@ describe('SearchController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('ProductsController.findAll', async () => {
+    let q = 'a5f023f2-30b4-4353-8fcd-3dab2646ed0b';
+    let products = {};
+
+    products = await controller.search(q);
+
+    expect(products).toEqual(mockProducts);
+    expect(prismaService.product.findMany).toHaveBeenCalledTimes(1);
+    expect(prismaService.product.findMany).toHaveBeenCalledWith({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: 'a5f023f2-30b4-4353-8fcd-3dab2646ed0b',
+              mode: 'insensitive',
+            },
+          },
+          {
+            code: {
+              contains: 'a5f023f2-30b4-4353-8fcd-3dab2646ed0b',
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+    });
   });
 });
